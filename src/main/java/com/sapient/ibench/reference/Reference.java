4e4c62ac1c5a710136a50b7f71c4703807a02a49
Marc Howell <sapient@microbump.com>
2014-12-20 01:48:49 +0200

    Bumped to version 1.0.4
    Fixes sever server side crash

--- src/main/java/com/sapient/ibench/reference/Reference.java
+++ src/main/java/com/sapient/ibench/reference/Reference.java
@@ -1,7 +1,7 @@
 package com.sapient.ibench.reference;
 
 public class Reference {
-    public static final String VERSION = "1.0.3";
+    public static final String VERSION = "1.0.4";
     public static final String MOD_ID = "ibench";
     public static final String MOD_NAME = "iBench";
 
