a3e74aced7fecd5f959e097e89bdbf302444647d
Marc Howell <sapient@microbump.com>
2014-12-25 09:55:11 +0200

    Bumped version

--- src/main/java/com/sapient/ibench/reference/Reference.java
+++ src/main/java/com/sapient/ibench/reference/Reference.java
@@ -1,7 +1,7 @@
 package com.sapient.ibench.reference;
 
 public class Reference {
-    public static final String VERSION = "1.0.8";
+    public static final String VERSION = "1.0.9";
     public static final String MOD_ID = "ibench";
     public static final String MOD_NAME = "iBench";
 
